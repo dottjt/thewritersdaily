ec823638c8a795c5986ec202f244481a42808c6c
dottjt <julius.reade@myob.com>
2020-05-30 11:08:23 +1000

    yes

--- util/index.ts
+++ util/index.ts
@@ -9,6 +9,7 @@ const main = async () => {
   const { generateModifiedRSS } = media
   const { episodesTWD } = data;
 
+  const PODCAST_WEBSITE = 'https://thewritersdaily.juliusreade.com/';
   // const TWD_RSS_URL = "http://rss.castbox.fm/everest/aab82e46f0cd4791b1c8ddc19d5158c3.xml"; // (this is the NFD one.)
   const TWD_RSS_URL = "http://rss.castbox.fm/everest/3f65d126b7e5499a8957e515501bb203.xml";
   const NEW_RSS_URL = "https://thewritersdaily.juliusreade.com/thewritersdaily_podcast.xml";
@@ -16,7 +17,6 @@ const main = async () => {
 
   const contentDirectory = path.join(__dirname, '../', 'content');
 
-  const PODCAST_WEBSITE = 'https://thewritersdaily.juliusreade.com/';
 
   await generateHugoMDFilesTWD({
     episodes: episodesTWD,
